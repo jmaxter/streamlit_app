c9a7d93e60ac6bffff9cb6946599e069db3db9e0
Jorge McDonald <38032234+jmaxter@users.noreply.github.com>
2024-10-22 14:15:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title("Hello World!")
+with st.sidebar:
+    st.header("About App")
+    st.write("This is my application.")
 
-st.write('Hello world!')
+st.header('This is a header with a divider', divider='rainbow')
+st.markdown("This is created with st.markdown")
+
+
+
+st.subheader("st.column")
+col1, col2 = st.columns(2)
+
+with col1:
+    x = st.slider("Choose an x value", 1, 10, )
+
+with col2:
+    st.write("The double value of :blue[***x***]**2 is", x**2)
+
+st.subheader("st.area_chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.area_chart(chart_data)
